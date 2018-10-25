1508364e48a0e82380b6093349327d6ce9c852da
github username <github email address>
2018-10-25 11:27:57 -0400

    added validations for notecard

--- app/models/notecard.rb
+++ app/models/notecard.rb
@@ -2,4 +2,9 @@ class Notecard < ApplicationRecord
   belongs_to :category
   belongs_to :user
   belongs_to :subcategory
+  validates :front, presence: true
+  validates :back, presence: true
+  validates :category_id, presence: true
+  validates :subcategory_id, presence: true
+
 end
